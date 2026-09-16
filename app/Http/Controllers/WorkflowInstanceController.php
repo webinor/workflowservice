4b74efe8c6c07d05b12c32461dca55634de659a6
Marcel <kgabinmarcel@gmail.com>
2026-09-16 16:18:26 +0100

    ui

--- app/Http/Controllers/WorkflowInstanceController.php
+++ app/Http/Controllers/WorkflowInstanceController.php
@@ -3743,8 +3743,8 @@ class WorkflowInstanceController extends Controller
              if ($data['uuid'] == "8b04c1fe-7e26-4bc6-992b-515404918eb1") {
 
 
-               throw new Exception(json_encode($fieldValue == $value));
-            //    throw new Exception(json_encode($documentData));
+            //    throw new Exception(json_encode($fieldValue == $value));
+               throw new Exception(json_encode($condition->operator));
             
         
         }
