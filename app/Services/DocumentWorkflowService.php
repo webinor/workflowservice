e7fbf8eaf91507ce8a703f5c5e49ef9a45aa185c
Marcel <kgabinmarcel@gmail.com>
2026-09-21 15:20:54 +0100

    ui

--- app/Services/DocumentWorkflowService.php
+++ app/Services/DocumentWorkflowService.php
@@ -1741,9 +1741,13 @@ foreach ($steps as $instanceStep) {
         if ($this->hasFinancialDocumentCityResponsibility($currentUserContext)) {
     // Le user possède une responsabilité financière liée à une ville
 
-       $assignmentPlace = strtoupper(
-        trim((string) data_get($doc, 'actor_details', ''))
-    );//assignment_place
+    //    $assignmentPlace = strtoupper(
+    //     trim((string) data_get($doc, 'actor_details.assignment_place', ''))
+    // );
+
+      $actor_details = 
+        ( data_get($doc, 'actor_details', ''))
+    ;//assignment_place
 
       $canViewFinancialDocYaounde =  (bool) data_get(
                 $currentUserContext,
@@ -1752,7 +1756,7 @@ foreach ($steps as $instanceStep) {
             );
 
         
-    throw new Exception(json_encode($assignmentPlace), 1);
+    throw new Exception(json_encode($actor_details), 1);
 
 
     if ($assignmentPlace == "YAOUNDE") {
